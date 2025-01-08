9220ad84617f9fc87eba6c21fd781acdbcdfb2ae
myry <wjq864247501@gmail.com>
2025-01-08 09:11:16 +0100

    update

--- 02.Software/01_Blink/src/main.cpp
+++ 02.Software/01_Blink/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include "Adafruit_NeoPixel.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN 48  
+#define LED_COUNT 1  
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void flashLED(void *param) {
+  strip.begin();
+  strip.show();
+
+  while(1) {
+    // r
+  strip.setPixelColor(0, strip.Color(255, 0, 0));
+  strip.show();
+  vTaskDelay(1000);
+
+  // g
+  strip.setPixelColor(0, strip.Color(0, 255, 0));
+  strip.show();
+  vTaskDelay(1000);
+
+  // b
+  strip.setPixelColor(0, strip.Color(0, 0, 255));
+  strip.show();
+  vTaskDelay(1000);
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  xTaskCreate(flashLED, "flash", 8192, NULL, 1, NULL);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
