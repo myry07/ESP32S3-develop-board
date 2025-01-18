03e7f5a2959d93936e0c7d3a09477856f9d19e88
myry <wjq864247501@gmail.com>
2025-01-18 18:44:54 +0100

    update

---
+++ 02.Software/02_ESP-IDF/01_Blink/main/01_Blink.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h" //操作gpio的头文件
+
+#define LED_GPIO GPIO_NUM_2
+
+void ledFlash(void *param)
+{
+    int gpio_level = 0;
+
+    while (1)
+    {
+        gpio_level = gpio_level ? 0 : 1;
+        gpio_set_level(LED_GPIO, gpio_level);
+        vTaskDelay(pdMS_TO_TICKS(500)); // 500ms
+    }
+}
+
+void app_main(void)
+{
+    // GPIO配置
+    gpio_config_t led_cfg = {
+        .pin_bit_mask = (1 << LED_GPIO),       // 按位掩码 可控制多个gpio
+        .pull_up_en = GPIO_PULLUP_DISABLE,     // 使能上拉
+        .pull_down_en = GPIO_PULLDOWN_DISABLE, // 使能下拉
+        .mode = GPIO_MODE_OUTPUT,              // 输出模式
+        .intr_type = GPIO_INTR_DISABLE,        // 中断
+    };
+
+    gpio_config(&led_cfg);
+
+    xTaskCreate(ledFlash, "led", 1024 * 2, NULL, 0, NULL);
+}
